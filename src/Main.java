904fab071f0621216e4c38e2df9658d3694b1140
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2025-04-12 22:07:59 +0200

    Update in the "Main" increasing the interactivity of the program

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,92 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int algChoice = 0;
 
-        System.out.println("Zgjedh algoritmin:");
-        System.out.println("1. Book Cipher");
-        System.out.println("2. Route Transposition");
-        int algChoice = sc.nextInt();
-        sc.nextLine();
+        while(true) {
+            if (algChoice == 0) {
+                System.out.println("\nZgjedh algoritmin:");
+                System.out.println("1. Book Cipher");
+                System.out.println("2. Route Transposition");
+                algChoice = sc.nextInt();
+                sc.nextLine();
 
-        System.out.println("Zgjedh veprimin:");
-        System.out.println("1. Enkriptim");
-        System.out.println("2. Dekriptim");
-        int action = sc.nextInt();
-        sc.nextLine();
-
-        switch (algChoice) {
-            case 1:
-                BookCipher bookCipher = new BookCipher();
-                if (action == 1) {
-                    System.out.println("Shkruaj mesazhin për enkriptim:");
-                    String msg = sc.nextLine();
-                    String encrypted = bookCipher.encrypt(msg);
-                    System.out.println("Enkriptuar: " + encrypted);
-                } else {
-                    System.out.println("Shkruaj tekstin për dekriptim:");
-                    String msg = sc.nextLine();
-                    String decrypted = bookCipher.decrypt(msg);
-                    System.out.println("Dekriptuar: " + decrypted);
+                if (algChoice != 1 && algChoice != 2) {
+                    System.out.println("❌ Zgjedhje e pavlefshme për algoritmin!");
+                    algChoice = 0;
+                    continue;
                 }
-                break;
+            }
 
-            case 2:
-                RouteTransposition route = new RouteTransposition();
-                if (action == 1) {
-                    System.out.println("Shkruaj mesazhin për enkriptim:");
-                    String msg = sc.nextLine();
-                    String encrypted = route.encrypt(msg);
-                    System.out.println("Enkriptuar: " + encrypted);
-                } else {
-                    System.out.println("Shkruaj tekstin për dekriptim:");
-                    String msg = sc.nextLine();
-                    String decrypted = route.decrypt(msg);
-                    System.out.println("Dekriptuar: " + decrypted);
-                }
-                break;
+            System.out.println("\nZgjedh veprimin:");
+            System.out.println("1. Enkriptim");
+            System.out.println("2. Dekriptim");
+            int action = sc.nextInt();
+            sc.nextLine();
 
-            default:
-                System.out.println("Zgjedhje e pavlefshme!");
+            if (action != 1 && action != 2) {
+                System.out.println("❌ Zgjedhje e pavlefshme për veprimin!");
+                continue;
+            }
+
+            switch (algChoice) {
+                case 1:
+                    System.out.println("\n--📖 Book Cipher--");
+                    BookCipher bookCipher = new BookCipher();
+                    if (action == 1) {
+                        System.out.println("Shkruaj mesazhin për enkriptim:");
+                        String msg = sc.nextLine();
+                        String encrypted = bookCipher.encrypt(msg);
+                        System.out.println("🔐 Enkriptuar: " + encrypted);
+                    } else {
+                        System.out.println("Shkruaj tekstin për dekriptim:");
+                        String msg = sc.nextLine();
+                        String decrypted = bookCipher.decrypt(msg);
+                        System.out.println("🔓 Dekriptuar: " + decrypted);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\n--🚗 Route Transposition--");
+                    RouteTransposition route = new RouteTransposition();
+                    if (action == 1) {
+                        System.out.println("Shkruaj mesazhin për enkriptim:");
+                        String msg = sc.nextLine();
+                        String encrypted = route.encrypt(msg);
+                        System.out.println("🔐 Enkriptuar: " + encrypted);
+                    } else {
+                        System.out.println("Shkruaj tekstin për dekriptim:");
+                        String msg = sc.nextLine();
+                        String decrypted = route.decrypt(msg);
+                        System.out.println("🔓 Dekriptuar: " + decrypted);
+                    }
+                    break;
+            }
+
+            System.out.println("\n➕ Çfarë dëshiron të bësh tani?");
+            System.out.println("1. Vazhdo me të njëjtin algoritëm");
+            System.out.println("2. Ndërro algoritmin");
+            System.out.println("3. Mbyll programin");
+
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            if (option == 1) {
+                continue;
+            } else if (option == 2) {
+                algChoice = 0;
+                continue;
+            } else if (option == 3) {
+                System.out.println("🔚 Programi u mbyll. Faleminderit!");
+                break;
+            } else {
+                System.out.println("⚠️ Zgjedhje e pavlefshme. Programi po mbyllet.");
+                break;
+            }
         }
+
         sc.close();
     }
 }
